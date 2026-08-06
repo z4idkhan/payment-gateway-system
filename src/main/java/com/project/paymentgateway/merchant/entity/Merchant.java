45905607e577bec6ae447f936eb88590254e8d94
Zaid Khan <khanzaid75005@gmail.com>
2026-08-06 13:22:28 +0530

    Fixed

--- src/main/java/com/project/paymentgateway/merchant/entity/Merchant.java
+++ src/main/java/com/project/paymentgateway/merchant/entity/Merchant.java
@@ -14,23 +14,22 @@ import java.util.UUID;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-
 public class Merchant {
 
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
 
-    @Column(name = "NAME", nullable = false, length = 200)
+    @Column(nullable = false, length = 200)
     private String name;
 
-    @Column(unique = true, length = 200)
+    @Column(unique = true, nullable = false)
     private String email;
 
     @Column(length = 20)
     private String contactNumber;
 
-    @Column(name = "BUSINESS_TYPE", length = 50)
+    @Column(length = 50)
     @Enumerated(EnumType.STRING)
     private BusinessType businessType;
 
@@ -40,23 +39,22 @@ public class Merchant {
     @Column(length = 200)
     private String websiteUrl;
 
-    @Column(nullable = false,length = 200)
+    @Column(length = 200, nullable = false)
     @Enumerated(EnumType.STRING)
-    private MerchantStatus status;
+    private MerchantStatus status = MerchantStatus.PENDING_KYC;
 
     @Column(length = 20)
     private String gstId;
 
-    @Column(length = 200)
+    @Column(length = 20)
     private String panId;
 
-    @Column(length = 20)
-    private String settelmentBankAccount;
+    @Column(length = 200)
+    private String settlementBankAccount;
 
     @Column(length = 20)
-    private String settelmentBankIfsc;
+    private String settlementBankIfsc;
 
     @Column(length = 200)
-    private String settelmentBankAccountHolderName;
-
+    private String settlementBankAccountHolderName;
 }
