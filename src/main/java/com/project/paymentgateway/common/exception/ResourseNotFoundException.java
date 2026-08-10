05b84a5bc35b762b0b78145252b92774a4829824
Zaid Khan <khanzaid75005@gmail.com>
2026-08-11 03:11:24 +0530

    Exception Handling Done

---
+++ src/main/java/com/project/paymentgateway/common/exception/ResourseNotFoundException.java
@@ -0,0 +1,7 @@
+package com.project.paymentgateway.common.exception;
+
+public class ResourseNotFoundException extends RuntimeException {
+    public ResourseNotFoundException(String message) {
+        super(message);
+    }
+}
