247b1383a1655a08a6aadd612d23698262985e3c
Zaid Khan <khanzaid75005@gmail.com>
2026-08-17 21:31:34 +0530

    Handeled Exception

--- src/main/java/com/project/paymentgateway/PaymentGateWayApplication.java
+++ src/main/java/com/project/paymentgateway/PaymentGateWayApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RazorpayApplication {
+public class PaymentGateWayApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(RazorpayApplication.class, args);
+		SpringApplication.run(PaymentGateWayApplication.class, args);
 	}
 
 }
--- src/main/java/com/project/paymentgateway/common/exception/ErrorResponse.java
+++ src/main/java/com/project/paymentgateway/common/exception/ErrorResponse.java
@@ -24,7 +24,3 @@ public record ErrorResponse (
     }
 
 }
-
-
-
-
