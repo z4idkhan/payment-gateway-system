59fb22b19a40dc0ae26ba6b586a484211b5a12db
Zaid Khan <khanzaid75005@gmail.com>
2026-08-28 21:36:02 +0530

    Handeled Api Key

--- src/main/java/com/project/paymentgateway/payment/repository/OrderRepository.java
+++ src/main/java/com/project/paymentgateway/payment/repository/OrderRepository.java
@@ -1,9 +1,12 @@
 package com.project.paymentgateway.payment.repository;
 
 import com.project.paymentgateway.payment.entity.OrderRecord;
+import jakarta.validation.constraints.Size;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
 
 public interface OrderRepository extends JpaRepository<OrderRecord, UUID> {
+
+    boolean existsByMerchantIdAndReceipt(UUID merchantId, @Size(max = 100) String receipt);
 }
--- src/main/java/com/project/paymentgateway/payment/service/impl/OrderServiceImpl.java
+++ src/main/java/com/project/paymentgateway/payment/service/impl/OrderServiceImpl.java
@@ -1,13 +1,29 @@
 package com.project.paymentgateway.payment.service.impl;
 
+import com.project.paymentgateway.common.exception.DuplicateResourceException;
 import com.project.paymentgateway.payment.dto.request.CreateOrderRequest;
 import com.project.paymentgateway.payment.dto.response.OrderResponse;
+import com.project.paymentgateway.payment.repository.OrderRepository;
 import com.project.paymentgateway.payment.service.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import java.util.UUID;
+
+@Service
 public class OrderServiceImpl implements OrderService {
+
+    @Autowired
+    OrderRepository orderRepository;
 
     @Override
     public OrderResponse create(CreateOrderRequest request) {
+        UUID merchantId = null;
+
+        if (orderRepository.existsByMerchantIdAndReceipt(merchantId, request.receipt())) {
+            throw new DuplicateResourceException("ORDER_WITH_THE_GIVEN_RECEIPT_ALREADY_EXISTS","Order with Reciept Already Exist");
+        }
+
         return null;
     }
 }
